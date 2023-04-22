99c40fb31e36d8a08898ac3245facadb55578ea9
Nicholas Peloeahae <nicholaspeloeahae@gmail.com>
2023-04-22 21:39:37 +0200

    created a login view form for authentification

--- to_do_list/base/urls.py
+++ to_do_list/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, LoginViewUser
 
 urlpatterns = [
+    path('login/', LoginViewUser.as_view(), name="login"),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='tasks'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
